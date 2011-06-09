ea6bad64fb79dc8a8b46454e277242d47e834431
D Malaxos <davidx@mindspring.com>
2011-06-08 22:09:17 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WackyApp::Application.config.secret_token = 'd6ff224a5f9fb3b45a89155714997a19828328e0edb49852025a9f009e0a36092e48e66ef087a89232e34f03982c0b85465f99cb97a306d5fa2342dabf686f63'
